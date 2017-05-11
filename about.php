5043f074d85b2443c3576acf3d8c14d805902558
Your Name <you@example.com>
2017-05-10 23:00:26 -0400

    php

---
+++ about.php
@@ -0,0 +1,55 @@
+
+<?php get_header(); ?>
+    
+<body>
+  <div class="navigation w-nav" data-animation="default" data-collapse="medium" data-duration="400">
+    <div class="container w-container">
+      <a class="brand w-nav-brand" href="#"><img class="img" src="images/unnamed.png" width="169">
+      </a>
+      <div class="share-buttons">
+        <a class="link-block w-inline-block" href="https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Fwww.theworldincontext.com%2F&amp;ref_src=twsrc%5Etfw&amp;text=Unbiased%20news%20directed%20to%20your%20inbox%20daily%&amp;tw_p=tweetbutton"><img sizes="70px" src="images/tweetbutton.png" srcset="images/tweetbutton-p-500x161.png 500w, images/tweetbutton.png 576w" width="70">
+        </a>
+      </div>
+      <div class="menu-nv"><a class="navlinks" href="index.html">Home</a><a class="navlinks" href="about.html">About</a>
+      </div>
+    </div>
+  </div>
+  <div class="be-centered reading-section">
+    <h1 class="full-widtgh"><em>What is The World in Context?<br><br></em></h1>
+    <p><strong>I started The World in Context out of
+frustration</strong>—with how difficult it is to just follow the news and be informed. There are so many poor sources of information, so many biases to consider.
+      <br>
+      <br><em>It’s almost like everyone has to be their own journalist. And that takes a lot of time—and a lot of
+effort.</em> &nbsp;
+      <br>
+      <br> Let me do that for you. I’ll bring you a daily rundown of the biggest news stories, important stuff that’s hidden beneath the headlines, and why it all matters—every Monday through Friday.
+      <br>
+      <br>I'll be as objective as possible, and do my best to add context to every story. I mark a clear divide between the news and my analysis, so you can draw your own conclusions.
+      <br>
+      <br>
+      <a href="old-home.html"><strong>Give The World in Context a try!</strong></a> I think you’ll find it useful. &nbsp;
+      <br>
+      <br>And if you do - spread it around to your friends. I'd greatly appreciate it!
+      <br>
+      <br>--
+      <br>
+      <br>Cheers,
+      <br>
+      <br>
+      <em>Brian Garrett-Glaser</em>
+      <br>
+      <br>
+      <em>Founder &amp; Writer,</em>
+      <br><em>The World in Context<br><br>(Disclosure: This project is 100% mine, and does not reflect the views of my employer, my girlfriend's cats, or anyone else. It's <a href="https://twitter.com/bgarrettglaser">just me!</a>)</em>
+    </p>
+  </div>
+  <div class="footer">
+    <div>2017 © The World in Context | Created with ❤ by <a href="https://micastudios.co" class="mica-studios">Mica Studios</a>
+    </div>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" type="text/javascript"></script>
+  <script src="js/theworldincontext.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+
+
+<?php get_footer(); ?>
